27f8e8ef197eb49c6ba981b962a18fc7ebeedf02
Santiago Pulgarin <s8pulgarin@hotmail.com>
2022-04-28 23:19:43 -0500

    fix basepath

--- main.py
+++ main.py
@@ -10,14 +10,12 @@ from routers import user_router
 
 from database import db
 
-BASE_PATH = '/api/v1'
-
 app = FastAPI()
 
 channel_model.Base.metadata.create_all(bind=db.engine)
 message_model.Base.metadata.create_all(bind=db.engine)
 user_model.Base.metadata.create_all(bind=db.engine)
 
-app.include_router(prefix=BASE_PATH, router=message_router.router, tags=['messages'])
-app.include_router(prefix=BASE_PATH, router=channel_router.router, tags=['channels'])
-app.include_router(prefix=BASE_PATH, router=user_router.router, tags=['users'])
+app.include_router(message_router.router, tags=['messages'])
+app.include_router(channel_router.router, tags=['channels'])
+app.include_router(user_router.router, tags=['users'])
