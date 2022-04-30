ceb49b66dbf06bc197967840e60ff392eb3e83bc
Santiago Pulgarin <s8pulgarin@hotmail.com>
2022-04-29 22:51:21 -0500

    remote postgresql db

--- database/db.py
+++ database/db.py
@@ -3,10 +3,8 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 engine = create_engine(
-    'sqlite:///men.sqlite',
-    echo=True,
-    connect_args={'check_same_thread': False}
-    )
+    'postgresql://txlxcpuyjviyrr:be9d5ec9f056998e6c7ddae76f6f72cbde05c1c9dd283bf8fabc1945894e4c37@ec2-52-71-69-66.compute-1.amazonaws.com:5432/d84a8okqkvk3pj'
+)
 
 
 SessionLocal = sessionmaker(
